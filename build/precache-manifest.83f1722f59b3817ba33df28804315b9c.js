4853bcb978f8041d4d9f636424de0b002739bfa1
charliehwang <charliehwang@gmail.com>
2022-06-17 03:00:56 +0000

    updated

---
+++ build/precache-manifest.83f1722f59b3817ba33df28804315b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9a58c558919b9abca609cf7e2d5347",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49575ebb9a679a395d7f",
+    "url": "/static/css/main.f8632e34.chunk.css"
+  },
+  {
+    "revision": "10dd3b54b9abe3e2243b",
+    "url": "/static/js/2.c73dd98f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c73dd98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "204ec5b78b97941192d3",
+    "url": "/static/js/3.5d48b696.chunk.js"
+  },
+  {
+    "revision": "49575ebb9a679a395d7f",
+    "url": "/static/js/main.fdb66596.chunk.js"
+  },
+  {
+    "revision": "f95994f8fcb818de7d71",
+    "url": "/static/js/runtime-main.c16ea55b.js"
+  }
+]);
